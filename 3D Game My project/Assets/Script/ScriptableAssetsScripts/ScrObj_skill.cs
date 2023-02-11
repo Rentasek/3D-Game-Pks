ddf460d2508f11d9937de83d68cfcc9bc807cd92
Remo <Remczas@interia.pl>
2023-02-12 00:14:04 +0100

    Skill/ScrObj_skill/Static_SkillForge -> Big Update
    Duży Update, zrezygnowałem z wielokrotnych zmiennych dla jednego spella (Skill: CastType, RangeType, EffectType itd jako Array), teraz jest po jednym CastType, RangeType, EffectType itd na każdy object Skill.
    Mechanika działa pozostały małe poprawki do ResourceType (HP, MP, Stam)

--- 3D Game My project/Assets/Script/ScriptableAssetsScripts/ScrObj_skill.cs
+++ 3D Game My project/Assets/Script/ScriptableAssetsScripts/ScrObj_skill.cs
@@ -1,4 +1,5 @@
 ﻿using Cinemachine;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -15,75 +16,75 @@ public class ScrObj_skill : ScriptableObject
     {
         castable, instant, hold
     }
-    [SerializeField] public Skill_CastingType[] skill_CastingType;
+    [SerializeField] public Skill_CastingType skill_CastingType;
 
     public enum Skill_RangeType
     {
         melee, cone, projectile, aoeMouse, self, summon
     }
-    [SerializeField] public Skill_RangeType[] skill_RangeType;
+    [SerializeField] public Skill_RangeType skill_RangeType;
 
     public enum Skill_EffectType
     {
         hit, boom, pierce, chain, dot, hot, heal, summon
     }
-    [SerializeField] public Skill_EffectType[] skill_EffectType;
+    [SerializeField] public Skill_EffectType skill_EffectType;
 
     public enum Skill_ResourceType
     {
         hp, mana, stamina
     }
-    [SerializeField] public Skill_ResourceType[] skill_ResourceType;
+    [SerializeField] public Skill_ResourceType skill_ResourceType;
 
 
     [Space]
-    [Header("CastingTypes")]
-    public int skill_CastingTypesCount; 
-    
+    //[Header("CastingTypes")]
+
+    /*[Tooltip("Skill input lokalny dla klasy skill"), SerializeField] public bool skill_input;
+    [Tooltip("Skill input Other lokalny dla klasy skill"), SerializeField] public bool skill_otherInput;*/
+
+
     [Header("SkillRange")]
-    [SerializeField] public float[] skill_MinRadius;
-    [SerializeField] public float[] skill_MinAngle;
-    [SerializeField] public float[] skill_MaxRadius;
-    [SerializeField] public float[] skill_MaxAngle;
-    [SerializeField] public int skill_RangeTypesCount;    
+    [Tooltip("Bazowy Minimalny Radius skilla"), SerializeField] public float skill_MinRadius;
+    [Tooltip("Bazowy Minimalny Kąt skilla"), SerializeField] public float skill_MinAngle;
+    [Tooltip("Bazowy Maxymalny Radius skilla"), SerializeField] public float skill_MaxRadius;
+    [Tooltip("Bazowy Maxymalny Kąt skilla"), SerializeField] public float skill_MaxAngle;
 
     [Header("SkillTime")]
-    [Tooltip("Ile czasu potrzeba do Max/Min Radius"), SerializeField] public float[] skill_TimeMaxRadius;
-    [Tooltip("Ile czasu potrzeba do Max/Min Angle"), SerializeField] public float[] skill_TimeMaxAngle;
-    [Tooltip("Ile czasu potrzeba do Deploy Cast"), SerializeField] public float[] skill_TimeCast;
+    [Tooltip("Ile czasu potrzeba do Max/Min Radius"), SerializeField] public float skill_TimeMaxRadius;
+    [Tooltip("Ile czasu potrzeba do Max/Min Angle"), SerializeField] public float skill_TimeMaxAngle;
+    [Tooltip("Ile czasu potrzeba do Deploy Cast"), SerializeField] public float skill_TimeCast;
 
-    [Header("EffectTypes")]
-    [SerializeField] public int skill_EffectTypesCount;
+    //[Header("EffectTypes")]
 
     [Header("SkillDamage/Cost")]
-    [SerializeField] public float[] skill_BaseResourceCost;
-    [SerializeField] public float[] skill_BaseDamage;
-    [SerializeField] public float[] skill_BaseCooldown;
-    [SerializeField] public float[] skill_Multiplier;
-    [SerializeField] public int skill_ResourceTypesCount;
+    [Tooltip("Bazowy Resource cost skilla"), SerializeField] public float skill_BaseResourceCost;
+    [Tooltip("Bazowy Damage skilla"), SerializeField] public float skill_BaseDamage;
+    [Tooltip("Bazowy Cooldown skilla"), SerializeField] public float skill_BaseCooldown;
+    [Tooltip("Multiplier do Levela i BonusCharStats do obliczeń [0.1f] -> na każdy lev i na każdy BonusDMG dodaje 0.1f BAZOWYCH DMG"), SerializeField] public float skill_Multiplier;
 
     [Header("SkillObstacles")]
-    [SerializeField] public LayerMask skill_ObstaclesMask; //Obstacles dla skilla
+    [Tooltip("Layer Mask z przeszkodami przez które nie da się atakować(z klasy skill)"), SerializeField] public LayerMask skill_ObstaclesMask; //Obstacles dla skilla
 
     [Header("SkillAudio")]    
-    [Tooltip("AudioClip skilla -> Caster"), SerializeField] public AudioClip[] skill_CastingAudioClip;    
-    [Tooltip("AudioClip skilla -> Target"), SerializeField] public AudioClip[] skill_OnHitAudioClip;
-    [Tooltip("VFX skilla -> Target"), SerializeField] public VisualEffect[] skill_OnHitVisualEffect;
-    [Tooltip("AudioVolume skilla -> Caster"), SerializeField] public float[] skill_CastingAudioVolume;
-    [Tooltip("AudioVolume skilla -> Target"), SerializeField] public float[] skill_OnHitAudioVolume;    
+    [CanBeNull, Tooltip("AudioClip skilla -> Caster"), SerializeField] public AudioClip skill_CastingAudioClip;    
+    [CanBeNull, Tooltip("AudioClip skilla -> Target"), SerializeField] public AudioClip skill_OnHitAudioClip;
+    [CanBeNull, Tooltip("VFX skilla -> Target"), SerializeField] public VisualEffect skill_OnHitVisualEffect;
+    [CanBeNull, Tooltip("AudioVolume skilla -> Caster"), SerializeField] public float skill_CastingAudioVolume;
+    [CanBeNull, Tooltip("AudioVolume skilla -> Target"), SerializeField] public float skill_OnHitAudioVolume;    
 
     [Header("Animator Values")]
-    [Tooltip("Jaki Float Animatora odpowiada za animację? - Nazwa"), SerializeField] public string skill_AnimatorFloat;
-    [Tooltip("Jaki Trigger Animatora odpowiada za animację?  - Nazwa"), SerializeField] public string skill_AnimatorTrigger;
-    [Tooltip("Jaki Bool Animatora odpowiada za animację? - Nazwa "), SerializeField] public string skill_AnimatorBool;
+    [CanBeNull, Tooltip("Jaki Float Animatora odpowiada za animację? - Nazwa"), SerializeField] public string skill_AnimatorFloat;
+    [CanBeNull, Tooltip("Jaki Trigger Animatora odpowiada za animację?  - Nazwa"), SerializeField] public string skill_AnimatorTrigger;
+    [CanBeNull, Tooltip("Jaki Bool Animatora odpowiada za animację? - Nazwa "), SerializeField] public string skill_AnimatorBool;
 
-    [Tooltip("Na którym combo colider działa?"), SerializeField] public float[] skill_onCombo;
+    [CanBeNull, Tooltip("Na którym combo colider działa?"), SerializeField] public float skill_onCombo;
 
-    [Header("SkillVariable VFX")]
-    [SerializeField] public float[] skill_ParticlesPerSec;
-    [SerializeField] public float[] skill_LifetimeMin;
-    [SerializeField] public float[] skill_LifetimeMax;
-    [SerializeField] public float[] skill_MovingTowardsFactor;
+    [Header("SkillVariable VFX - niepodpięte")]
+    [Tooltip("VFX - Ilość particali"), SerializeField] public float skill_ParticlesPerSec;
+    [Tooltip("VFX - Minimalny Lifetime particali"), SerializeField] public float skill_LifetimeMin;
+    [Tooltip("VFX - Maxymalny Lifetime particali"), SerializeField] public float skill_LifetimeMax;
+    [Tooltip("VFX - Kierunek i prędkość (wektor) rozchodzenia się particali"), SerializeField] public float skill_MovingTowardsFactor;
 
 }
 
--- 3D Game My project/Assets/Script/Spells/Skill.cs
+++ 3D Game My project/Assets/Script/Spells/Skill.cs
@@ -2,190 +2,214 @@
 using JetBrains.Annotations;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class Skill : MonoBehaviour
 {
-    [SerializeField] public ScrObj_skill scrObj_Skill;
-    [SerializeField] public CharacterStatus live_charStats;
-    [SerializeField] public CharacterBonusStats currentCharacterBonusStats;
-
+    [Tooltip("Scriptable Object Skilla"), SerializeField] public ScrObj_skill scrObj_Skill;
+    [Tooltip("Live_charStats Castera"), SerializeField] public CharacterStatus live_charStats;
+    [Tooltip("Bonus_charStats Castera"), SerializeField] public CharacterBonusStats currentCharacterBonusStats;
+    [Tooltip("Ten GameObject skill"), SerializeField] public Skill skill;
 
     [Header("Testing/Debug")]
-    [SerializeField] public bool skill_input;
-    [SerializeField] public bool skill_otherInput;
-    [SerializeField] public float skill_currentCastingProgress;
-    [SerializeField] public bool skill_currentCastingFinished;
-    [SerializeField] public bool skill_currentCastingInstant;
+    [Tooltip("Skill input lokalny dla klasy skill, Czy INPUTUJE casta?"), SerializeField] public bool skill_input;
+    [Tooltip("Skill input Other lokalny dla klasy skill"), SerializeField] public bool skill_otherInput;
+    [Tooltip("Aktualny progress Castowania (CastingType -> Castable)"), SerializeField] public float skill_currentCastingProgress;
+    [Tooltip("Bool zwracany true jeśli progress castowania dojdzie do 100% (CastingType -> Castable)"), SerializeField] public bool skill_currentCastingFinished;
+    [Tooltip("Bool zwracany true jeśli jest instantCast (CastingType -> Instant)"), SerializeField] public bool skill_currentCastingInstant;
     
     [Header("Targets")]
-    [CanBeNull, SerializeField] private List<Collider>[] skill_targetColliders;
-    [SerializeField, TagField] public string[] skill_EnemiesArray; //Pozwala na wybór Enemies przy pomocy Tag     
-    [SerializeField] public GameObject[] skill_casterGameobject;
+    [Tooltip("Zwracana lista colliderów zgodnych z parametrami(EnemyTag,InCurrentRadius,InCurrentAngle)"), CanBeNull, SerializeField] public List<Collider> skill_targetColliders;
+    [Tooltip("Enemies Array z klasy skill(do bazowego enemies array dopisane Destructibles, (Metoda EnemyArraySelector)"), SerializeField, TagField] public string[] skill_EnemiesArray; //Pozwala na wybór Enemies przy pomocy Tag     
+    [Tooltip("GameObject skilla (castera) -> potrzebny do transform"), SerializeField] public GameObject skill_casterGameobject;
 
     [Header("Skill Cone Settings")]
-    [CanBeNull, SerializeField] private bool[] skill_targetInRange;
-    [CanBeNull, SerializeField] private bool[] skill_targetInAngle;
+    [Tooltip("Bool zwracający czy w Range jest przeciwnik"), CanBeNull, SerializeField] public bool skill_targetInRange;
+    [Tooltip("Bool zwracający czy w Angle(i Range) jest przeciwnik"), CanBeNull, SerializeField] public bool skill_targetInAngle;
     [Space]
-    [CanBeNull, SerializeField] private float[] skill_currentRadius;
-    [CanBeNull, SerializeField] private float[] skill_currentAngle;
+    [Tooltip("Aktualny Radius skilla"), CanBeNull, SerializeField] public float skill_currentRadius;
+    [Tooltip("Aktualny Kąt skilla"), CanBeNull, SerializeField] public float skill_currentAngle;
     [Space]
-    [CanBeNull, SerializeField] private float[] skill_currentVectorRadius;
-    [CanBeNull, SerializeField] private float[] skill_currentVectorAngle;
+    [Tooltip("(ref/refrence) Aktualny wektor(kierunek) w którum porusza się currentRadius skilla"), CanBeNull, SerializeField] public float skill_currentVectorRadius;
+    [Tooltip("(ref/refrence) Aktualny wektor(kierunek) w którum porusza się currentAngle skilla"), CanBeNull, SerializeField] public float skill_currentVectorAngle;
 
     [Header("SkillDamage/Cost - current")]
-    [SerializeField] public float[] skill_currentResourceCost;
-    [SerializeField] public float[] skill_currentResourceType; //HP,Mana,Stamina value (float)
-    [SerializeField] public float[] skill_currentDamage;
-    [SerializeField] public float[] skill_currentCooldown;
-
-    [Header("Current Types")]
-    [SerializeField] public int currentRangeType; //trzyma informacje o numerze currentRangeTtype
-    [SerializeField] public int currentResourceType; //trzyma informacje o numerze currentRangeTtype
-    [SerializeField] public int currentCastingType; //trzyma informacje o numerze currentRangeTtype
-    [SerializeField] public int currentEffectType; //trzyma informacje o numerze currentRangeTtype
-
+    [Tooltip("Aktualny Resource cost skilla"), SerializeField] public float skill_currentResourceCost;
+    [Tooltip("Resource type dla Skilla (HP,Mana,Stamina value (float)) -> z live_charStats"), SerializeField] public float skill_currentResource;
+    [Tooltip("Aktualny Damage skilla (zwracany po przeliczeniu)"), SerializeField] public float skill_currentDamage;
+    [Tooltip("Aktualny Cooldown skilla (zwracany po przeliczeniu)"), SerializeField] public float skill_currentCooldown;
+   
     [Header("Utils")]
-    [Tooltip("AudioSource skilla -> Caster"), SerializeField] public AudioSource[] skill_AudioSource;
-    [Tooltip("VFX skilla -> Caster"), SerializeField] public VisualEffect[] skill_CastingVisualEffect;
+    [Tooltip("AudioSource skilla -> Caster"), SerializeField] public AudioSource skill_AudioSource;
+    [Tooltip("VFX skilla -> Caster"), SerializeField] public VisualEffect skill_CastingVisualEffect;
 
     private void OnValidate()
     {
-        skill_EnemiesArray = Static_SkillForge.EnemyArraySelector(live_charStats.currentEnemiesArray);        
+        skill = this;
+        skill_EnemiesArray = Static_SkillForge.EnemyArraySelector(live_charStats.currentEnemiesArray);
+
+        
+
+        Skill_Prepare();
     }
 
     private void FixedUpdate()
     {
-        if (live_charStats.inputPrimary || live_charStats.inputSecondary) { Skill_Prepare(); } //Jeśli używa jakiegokolwiek inputa -> Skill_Prepare (tak żeby nie preparował non-stop)       
+        Static_SkillForge.ResourceTypeSelector_In(scrObj_Skill, skill, live_charStats);
 
+        
+        //Skill_Prepare();
         Skill_Casting(scrObj_Skill);
 
         Skill_Range(scrObj_Skill);
 
         Skill_Effect(scrObj_Skill);
+
+        //if (live_charStats.isCasting) { ResourceTypeSelectorRefrence(); }
+
+        Static_SkillForge.ResourceTypeSelector_Out(scrObj_Skill, skill, live_charStats);
+
+
+
     }
 
-    public void Skill_Prepare()
+    /*public void ResourceTypeSelector()
     {
-        Static_SkillForge.InputSelector(scrObj_Skill.skill_InputCastingType, skill_input, skill_otherInput, live_charStats.inputPrimary, live_charStats.inputSecondary);
-
-        for (int i = 0; i < scrObj_Skill.skill_ResourceTypesCount; i++)
+        switch (scrObj_Skill.skill_ResourceType)
         {
-            Static_SkillForge.ResourceTypeSelector(scrObj_Skill.skill_ResourceType[i], live_charStats.currentHP, live_charStats.currentMP, live_charStats.currentStam, skill_currentResourceType[i]);
-        }
-        for (int i = 0; i < scrObj_Skill.skill_EffectTypesCount; i++)
-        {
-            Static_SkillForge.Skill_ValuesUpdate(skill_currentDamage[i], skill_currentResourceCost[i], skill_currentCooldown[i], scrObj_Skill.skill_BaseDamage[i], scrObj_Skill.skill_BaseResourceCost[i],
-                scrObj_Skill.skill_BaseCooldown[i], scrObj_Skill.skill_Multiplier[i], live_charStats.currentCharLevel, currentCharacterBonusStats.bonus_Skill_Damage);
-        }
-       
+            case ScrObj_skill.Skill_ResourceType.hp:
+                ResourceTypeRefrence(ref live_charStats.currentHP);
+                break;
 
+            case ScrObj_skill.Skill_ResourceType.mana:
+                ResourceTypeRefrence(ref live_charStats.currentMP);
+                break;
+
+            case ScrObj_skill.Skill_ResourceType.stamina:
+                ResourceTypeRefrence(ref live_charStats.currentStam);
+                break;
+        }
+    }
+
+
+    public void ResourceTypeSelectorRefrence()
+    {
+        switch(scrObj_Skill.skill_ResourceType)
+        {
+            case ScrObj_skill.Skill_ResourceType.hp:
+                skill.skill_currentResource = live_charStats.currentHP;
+            break;
+
+            case ScrObj_skill.Skill_ResourceType.mana:
+                skill.skill_currentResource = live_charStats.currentMP;
+            break;
+
+            case ScrObj_skill.Skill_ResourceType.stamina:
+                skill.skill_currentResource = live_charStats.currentStam;
+            break;
+        }
+    }
+
+    public void ResourceTypeRefrence(ref float skill_currentResource)
+    {
+        skill_currentResource = skill.skill_currentResource;
+    }*/
+
+    public void Skill_Prepare()
+    {        
         
+
+        Static_SkillForge.Skill_ValuesUpdate(scrObj_Skill, skill, live_charStats, currentCharacterBonusStats);
+
     }
 
     public void Skill_Casting(ScrObj_skill scrObj_Skill)
     { 
-        live_charStats.skill_CanCast = !skill_otherInput && !live_charStats.isRunning && live_charStats.currentMoveSpeed != live_charStats.currentRunSpeed;
+        live_charStats.skill_CanCast = !skill.skill_otherInput && !live_charStats.isRunning && live_charStats.currentMoveSpeed != live_charStats.currentRunSpeed;
         
         if (live_charStats.skill_CanCast)
         {
-            for (currentCastingType = 0; currentCastingType < scrObj_Skill.skill_CastingTypesCount; currentCastingType++)
+            switch (scrObj_Skill.skill_CastingType)
             {
-                switch (scrObj_Skill.skill_CastingType[currentCastingType])
-                {
-                    case ScrObj_skill.Skill_CastingType.castable:
-                        Static_SkillForge.Skill_Castable_VFX_Audio(skill_input, live_charStats.isCasting, live_charStats.skill_CanCast, skill_currentResourceType[currentCastingType], skill_currentResourceCost[currentCastingType], skill_currentCastingProgress,
-                            skill_currentCastingFinished, scrObj_Skill.skill_TimeCast[currentCastingType], skill_CastingVisualEffect[currentCastingType],
-                            skill_AudioSource[currentCastingType], scrObj_Skill.skill_CastingAudioClip[currentCastingType], scrObj_Skill.skill_CastingAudioVolume[currentCastingType], live_charStats.currentAnimator, scrObj_Skill.skill_AnimatorBool);
-                        break;
+                case ScrObj_skill.Skill_CastingType.castable:
+                    Static_SkillForge.Skill_Castable_VFX_Audio(scrObj_Skill, skill, live_charStats);
+                    break;
 
-                    case ScrObj_skill.Skill_CastingType.instant:
-                        Static_SkillForge.Skill_Instant_VFX_Audio(skill_input, live_charStats.isCasting, live_charStats.skill_CanCast, skill_currentResourceType[currentCastingType], skill_currentResourceCost[currentCastingType], skill_currentCastingInstant,
-                            skill_CastingVisualEffect[currentCastingType], skill_AudioSource[currentCastingType], scrObj_Skill.skill_CastingAudioClip[currentCastingType], scrObj_Skill.skill_CastingAudioVolume[currentCastingType], live_charStats.currentAnimator, scrObj_Skill.skill_AnimatorTrigger);
-                        break;
+                case ScrObj_skill.Skill_CastingType.instant:
+                    Static_SkillForge.Skill_Instant_VFX_Audio(scrObj_Skill, skill, live_charStats);
+                    break;
 
-                    case ScrObj_skill.Skill_CastingType.hold:
-                        Static_SkillForge.Skill_Hold_VFX_Audio(skill_input, live_charStats.isCasting, live_charStats.skill_CanCast, skill_currentResourceType[currentCastingType], skill_currentResourceCost[currentCastingType], skill_CastingVisualEffect[currentCastingType],
-                            skill_AudioSource[currentCastingType], scrObj_Skill.skill_CastingAudioClip[currentCastingType], scrObj_Skill.skill_CastingAudioVolume[currentCastingType], live_charStats.currentAnimator, scrObj_Skill.skill_AnimatorBool);
-                        break;
-                }
-            }           
+                case ScrObj_skill.Skill_CastingType.hold:
+                    Static_SkillForge.Skill_Hold_VFX_Audio(scrObj_Skill, skill, live_charStats);
+                    break;
+            }
         }
     }    
 
     public void Skill_Range(ScrObj_skill scrObj_Skill)
     {
-        if (!live_charStats.isDead && skill_input && live_charStats.skill_CanCast) //tylko dla żywych :P
+        if (!live_charStats.isDead && live_charStats.skill_CanCast) //tylko dla żywych :P
         {
-            for (currentRangeType = 0; currentRangeType < scrObj_Skill.skill_RangeTypesCount; currentRangeType++)
+            switch (scrObj_Skill.skill_RangeType)
             {
-                switch (scrObj_Skill.skill_RangeType[currentRangeType])
-                {
-                    case ScrObj_skill.Skill_RangeType.melee:
+                case ScrObj_skill.Skill_RangeType.melee:
 
-                        break;
+                    break;
 
-                    case ScrObj_skill.Skill_RangeType.cone:
-                        Static_SkillForge.Skill_Cone_AttackConeCheck(live_charStats.isCasting, skill_currentRadius[currentRangeType], skill_currentAngle[currentRangeType], scrObj_Skill.skill_MinRadius[currentRangeType],
-                            scrObj_Skill.skill_MaxRadius[currentRangeType], scrObj_Skill.skill_MinAngle[currentRangeType], scrObj_Skill.skill_MaxAngle[currentRangeType], skill_currentVectorRadius[currentRangeType],
-                            skill_currentVectorAngle[currentRangeType], scrObj_Skill.skill_TimeMaxRadius[currentRangeType], scrObj_Skill.skill_TimeMaxAngle[currentRangeType], skill_casterGameobject[currentRangeType],
-                            live_charStats.currentEnemiesArray, skill_targetInRange[currentRangeType], skill_targetInAngle[currentRangeType], scrObj_Skill.skill_ObstaclesMask, skill_targetColliders[currentRangeType]);
-                        break;
+                case ScrObj_skill.Skill_RangeType.cone:
+                    Static_SkillForge.Skill_Cone_AttackConeCheck(scrObj_Skill, skill, live_charStats);
+                    break;
 
-                    case ScrObj_skill.Skill_RangeType.projectile:
+                case ScrObj_skill.Skill_RangeType.projectile:
 
-                        break;
+                    break;
 
-                    case ScrObj_skill.Skill_RangeType.aoeMouse:
+                case ScrObj_skill.Skill_RangeType.aoeMouse:
 
-                        break;
+                    break;
 
-                    case ScrObj_skill.Skill_RangeType.self:
+                case ScrObj_skill.Skill_RangeType.self:
 
-                        break;
+                    break;
 
-                    case ScrObj_skill.Skill_RangeType.summon:
+                case ScrObj_skill.Skill_RangeType.summon:
 
-                        break;
-                }
+                    break;
             }
         }
     }
 
     public void Skill_Effect(ScrObj_skill scrObj_Skill)
     {
-        for (currentEffectType = 0; currentEffectType < scrObj_Skill.skill_EffectTypesCount; currentEffectType++)
+        switch (scrObj_Skill.skill_EffectType)
         {
-            switch (scrObj_Skill.skill_EffectType[currentEffectType])
-            {
-                case ScrObj_skill.Skill_EffectType.hit:                    
-                    Static_SkillForge.Skill_Cone_Damage(live_charStats.isCasting, skill_targetColliders[currentRangeType], live_charStats.currentXP, live_charStats.currentMP, skill_currentDamage[currentEffectType],
-                        live_charStats.currentCharLevel, skill_currentResourceCost[currentResourceType]);
-                    break;
+            case ScrObj_skill.Skill_EffectType.hit:
+                Static_SkillForge.Skill_TargetList_DamageOverTime(skill, live_charStats);
+                break;
 
-                case ScrObj_skill.Skill_EffectType.boom:
-                    break;
+            case ScrObj_skill.Skill_EffectType.boom:
+                break;
 
-                case ScrObj_skill.Skill_EffectType.pierce:
-                    break;
+            case ScrObj_skill.Skill_EffectType.pierce:
+                break;
 
-                case ScrObj_skill.Skill_EffectType.chain:
-                    break;
+            case ScrObj_skill.Skill_EffectType.chain:
+                break;
 
-                case ScrObj_skill.Skill_EffectType.dot:
-                    break;
+            case ScrObj_skill.Skill_EffectType.dot:
+                break;
 
-                case ScrObj_skill.Skill_EffectType.hot:
-                    break;
-                    
-                case ScrObj_skill.Skill_EffectType.heal:
-                    break;
-                
-                case ScrObj_skill.Skill_EffectType.summon:
-                    break;
-            }
+            case ScrObj_skill.Skill_EffectType.hot:
+                break;
+
+            case ScrObj_skill.Skill_EffectType.heal:
+                break;
+
+            case ScrObj_skill.Skill_EffectType.summon:
+                break;
         }
     }
 
@@ -198,7 +222,7 @@ public class Skill : MonoBehaviour
 
     private void OnDrawGizmos() //rusyje wszystkie
     {
-        //GizmosDrawer();
+        GizmosDrawer();
     }
     /*private void OnDrawGizmosSelected() //rysuje tylko zaznaczone
     {
@@ -207,22 +231,19 @@ public class Skill : MonoBehaviour
 
     private void GizmosDrawer()
     {
-        for (int j = 0; j < scrObj_Skill.skill_RangeTypesCount; j++)
+        Handles.color = live_charStats.spell_breathAngleColor;
+        Handles.DrawSolidArc(transform.position, Vector3.up, Quaternion.AngleAxis(-(skill_currentAngle / 2), Vector3.up) * transform.forward, skill_currentAngle, skill_currentRadius); //rysuje coneAngle view               
+
+
+        if (skill_targetInRange && skill_targetInAngle)
         {
-            Handles.color = live_charStats.spell_breathAngleColor;
-            Handles.DrawSolidArc(transform.position, Vector3.up, Quaternion.AngleAxis(-(skill_currentAngle[j] / 2), Vector3.up) * transform.forward, skill_currentAngle[j], skill_currentRadius[j]); //rysuje coneAngle view               
-
-
-            if (skill_targetInRange[j] && skill_targetInAngle[j])
+            Handles.color = live_charStats.spell_breathRaycastColor;
+            for (int i = 0; i < skill_targetColliders.Count; i++)
             {
-                Handles.color = live_charStats.spell_breathRaycastColor;
-                for (int i = 0; i < skill_targetColliders[j].Count; i++)
-                {
-                    Handles.DrawLine(transform.position, skill_targetColliders[j][i].transform.position, live_charStats.fov_editorLineThickness); //rysowanie lini w kierunku targetów breatha jeśli nie zasłania go obstacle Layer
-                }
-
+                Handles.DrawLine(transform.position, skill_targetColliders[i].transform.position, live_charStats.fov_editorLineThickness); //rysowanie lini w kierunku targetów breatha jeśli nie zasłania go obstacle Layer
             }
-        }       
+
+        }
     }
 
 #endif
