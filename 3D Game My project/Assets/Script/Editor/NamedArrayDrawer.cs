4aa07171382896fb614ad7c3720779821507b8b6
Remo <Remczas@interia.pl>
2023-02-13 22:21:32 +0100

    NamedArrayAttribute i NamedArrayDrawer
    Custom Editor do wyświetlania w ScrObj_Skilla -> Arraya Enumeratorów TargetType z Fieldami podpisanymi
    CastableFinished, Instant i Hold odpowiadającymi za rodzaje Inputu

---
+++ 3D Game My project/Assets/Script/Editor/NamedArrayDrawer.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(NamedArrayAttribute))]public class NamedArrayDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        try
+        {
+            int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            EditorGUI.ObjectField(rect, property, new GUIContent(((NamedArrayAttribute)attribute).names[pos]));
+        }
+        catch 
+        {
+            EditorGUI.ObjectField(rect, property, label);
+        }
+    }
+}
+
+[CustomPropertyDrawer(typeof(EnumNamedArrayAttribute))]
+public class DrawwerEnumNamedArray : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EnumNamedArrayAttribute enumNames = attribute as EnumNamedArrayAttribute;  //Tworzenie lokalnej (zmiennej / instancji klasy) enumNames i przypisywanie attribute z EnumNamedArrayAttribute
+       
+        
+        //propertyPath returns something like component_hp_max.Array.data[4]        
+        //so get the index from there
+        int index = System.Convert.ToInt32(property.propertyPath.Substring(property.propertyPath.IndexOf("[")).Replace("[", "").Replace("]", "")); //Pobieranie Indexu z enuma
+        
+        //change the label
+        label.text = enumNames.names[index]; //tutaj następuje już zmiana (Element 0, 1, 2, itd) na nazwę enuma o indexie[index]
+        
+        //draw field
+        EditorGUI.PropertyField(position, property, label, true); //Rysowanie samego property Fielda
+    }
+}
+
+
---
+++ 3D Game My project/Assets/Script/Utils/NamedArrayAttribute.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class NamedArrayAttribute : PropertyAttribute
+{
+    public readonly string[] names;
+    public NamedArrayAttribute(string[] names)
+    {
+        this.names = names;
+    }
+}
+
+public class EnumNamedArrayAttribute : PropertyAttribute
+{    
+    public string[] names;
+    
+    public EnumNamedArrayAttribute(System.Type names_enum_type)     
+    {
+        this.names = System.Enum.GetNames(names_enum_type);    //Tutaj pobiera name do fieldów z Enuma wskazanego przy wpisywaniu zmiennej [EnumNamedArray(typeof(names_enum_type))]
+    }
+}
+
+
