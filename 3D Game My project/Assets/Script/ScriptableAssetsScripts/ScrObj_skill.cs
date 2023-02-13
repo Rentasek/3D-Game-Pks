dc15d8913cf2936c8eae80157e9148f495d34142
Remo <Remczas@interia.pl>
2023-02-13 22:23:50 +0100

    Update ScrObj_skill.cs
    Foramtowanie tekstu i wrzucenie CustomPropertyDrawera dla enumeratora Skill_EffectTypeArray, tak żeby wyświetlały się nazwy z InputType

--- 3D Game My project/Assets/Script/ScriptableAssetsScripts/ScrObj_skill.cs
+++ 3D Game My project/Assets/Script/ScriptableAssetsScripts/ScrObj_skill.cs
@@ -1,40 +1,31 @@
 ﻿using Cinemachine;
 using JetBrains.Annotations;
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.VFX;
 
 [CreateAssetMenu(fileName = "SpellInfo", menuName = "Chars/SpellInfo")]
 public class ScrObj_skill : ScriptableObject
 {
-    public enum Skill_InputType
-    {
-        primary, secondary
-    }
+    public enum Skill_InputType { primary, secondary }
     [SerializeField] public Skill_InputType skill_InputType;
 
-    public enum Skill_CastingType
-    {
-        castable, instant, hold
-    }
-    [SerializeField] public Skill_CastingType skill_CastingType;
+    public enum Skill_CastingType { Castable, Instant, Hold }
+    [SerializeField, HideInInspector] public Skill_CastingType skill_CastingType;
 
-    public enum Skill_RangeType
-    {
-        melee, cone, projectile, aoeMouse, self, summon
-    }
+    public enum Skill_RangeType { melee, cone, projectile, aoeMouse, self, summon }
     [SerializeField] public Skill_RangeType skill_RangeType;
 
-    public enum Skill_EffectType
-    {
-        hit, boom, pierce, chain, damageOverTime, healOverTime, heal, summon
-    }
-    [SerializeField] public Skill_EffectType skill_EffectType;
+    public enum Skill_EffectType { hit, boom, pierce, chain, damageOverTime, healOverTime, heal, summon }
+    [SerializeField, HideInInspector] public Skill_EffectType skill_EffectType;
 
-    public enum Skill_ResourceType
-    {
-        health, mana, stamina
-    }
+    public enum Skill_ResourceType { health, mana, stamina }    
     [SerializeField] public Skill_ResourceType skill_ResourceType;
+
+
+    public enum Skill_EffectTypeArray { none, hit, boom, pierce, chain, damageOverTime, healOverTime, heal, summon }
+    [SerializeField, EnumNamedArray(typeof(Skill_CastingType))] public Skill_EffectTypeArray[] skill_EffectTypeArray = new Skill_EffectTypeArray[3]; //Max 3 elementy
 
 
     [Space]
@@ -48,11 +39,11 @@ public class ScrObj_skill : ScriptableObject
     [Tooltip("Ile czasu potrzeba do Max/Min Radius"), SerializeField] public float skill_TimeMaxRadius;
     [Tooltip("Ile czasu potrzeba do Max/Min Angle"), SerializeField] public float skill_TimeMaxAngle;
     [Tooltip("Ile czasu potrzeba do Deploy Cast"), SerializeField] public float skill_TimeCast;
-    
+    [Tooltip("Bazowy Cooldown skilla"), SerializeField] public float skill_BaseCooldown;
+
     [Header("SkillDamage/Cost")]
     [Tooltip("Bazowy Resource cost skilla"), SerializeField] public float skill_BaseResourceCost;
-    [Tooltip("Bazowy Damage skilla"), SerializeField] public float skill_BaseDamage;
-    [Tooltip("Bazowy Cooldown skilla"), SerializeField] public float skill_BaseCooldown;
+    [Tooltip("Bazowy Damage skilla"), SerializeField] public float skill_BaseDamage;    
     [Tooltip("Multiplier do Levela i BonusCharStats do obliczeń [0.1f] -> na każdy lev i na każdy BonusDMG dodaje 0.1f BAZOWYCH DMG"), SerializeField] public float skill_Multiplier;
 
     [Header("SkillObstacles")]
@@ -66,18 +57,16 @@ public class ScrObj_skill : ScriptableObject
     [CanBeNull, Tooltip("AudioVolume skilla -> Target"), SerializeField] public float skill_OnHitAudioVolume;    
 
     [Header("Animator Values")]
-    [CanBeNull, Tooltip("Jaki Float Animatora odpowiada za animację? - Nazwa"), SerializeField] public string skill_AnimatorFloat;
-    [CanBeNull, Tooltip("Jaki Trigger Animatora odpowiada za animację?  - Nazwa"), SerializeField] public string skill_AnimatorTrigger;
-    [CanBeNull, Tooltip("Jaki Bool Animatora odpowiada za animację? - Nazwa "), SerializeField] public string skill_AnimatorBool;
+    [CanBeNull, Tooltip("Jaki Float Animatora odpowiada za animację? - Nazwa"), SerializeField] public string skill_AnimatorFloatName;
+    [CanBeNull, Tooltip("Jaki Trigger Animatora odpowiada za animację?  - Nazwa"), SerializeField] public string skill_AnimatorTriggerName;
+    [CanBeNull, Tooltip("Jaki Bool Animatora odpowiada za animację? - Nazwa "), SerializeField] public string skill_AnimatorBoolName;
 
-    [CanBeNull, Tooltip("Na którym combo colider działa?"), SerializeField] public float skill_onCombo;
+   /* [CanBeNull, Tooltip("Na którym combo colider działa?"), SerializeField] public float skill_onCombo;
 
-    [Header("SkillVariable VFX - niepodpięte")]
+    [Header("SkillVariable VFX")]
     [Tooltip("VFX - Ilość particali"), SerializeField] public float skill_ParticlesPerSec;
     [Tooltip("VFX - Minimalny Lifetime particali"), SerializeField] public float skill_LifetimeMin;
     [Tooltip("VFX - Maxymalny Lifetime particali"), SerializeField] public float skill_LifetimeMax;
-    [Tooltip("VFX - Kierunek i prędkość (wektor) rozchodzenia się particali"), SerializeField] public float skill_MovingTowardsFactor;
+    [Tooltip("VFX - Kierunek i prędkość (wektor) rozchodzenia się particali"), SerializeField] public float skill_MovingTowardsFactor;*/
 
 }
-
-
