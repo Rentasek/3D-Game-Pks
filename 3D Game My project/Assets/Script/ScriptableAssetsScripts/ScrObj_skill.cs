a08c0d065183d2277bf59d9b448567f11ffd43ec
Remo <Remczas@interia.pl>
2023-02-14 13:47:56 +0100

    Update ScrObj_skill.cs
    Zmiany nazw zmiennych + dorobienie Nowych zmiennych do obsługi Animatora

--- 3D Game My project/Assets/Script/ScriptableAssetsScripts/ScrObj_skill.cs
+++ 3D Game My project/Assets/Script/ScriptableAssetsScripts/ScrObj_skill.cs
@@ -50,23 +50,35 @@ public class ScrObj_skill : ScriptableObject
     [Tooltip("Layer Mask z przeszkodami przez które nie da się atakować(z klasy skill)"), SerializeField] public LayerMask skill_ObstaclesMask; //Obstacles dla skilla
     
     [Header("SkillAudio")]    
-    [CanBeNull, Tooltip("AudioClip skilla -> Caster"), SerializeField] public AudioClip skill_CastingAudioClip;    
-    [CanBeNull, Tooltip("AudioClip skilla -> Target"), SerializeField] public AudioClip skill_OnHitAudioClip;
-    [CanBeNull, Tooltip("VFX skilla -> Target"), SerializeField] public VisualEffect skill_OnHitVisualEffect;
+    [CanBeNull, Tooltip("AudioClip skilla -> Caster"), SerializeField] public AudioClip skill_CastingAudioClip;
+    [CanBeNull, Tooltip("AudioClip skilla OnFinished -> Caster"), SerializeField] public AudioClip skill_OnFinishCastingAudioClip;
+    [CanBeNull, Tooltip("AudioClip skilla -> Target"), SerializeField] public AudioClip skill_OnTargetHitAudioClip;
     [CanBeNull, Tooltip("AudioVolume skilla -> Caster"), SerializeField] public float skill_CastingAudioVolume;
-    [CanBeNull, Tooltip("AudioVolume skilla -> Target"), SerializeField] public float skill_OnHitAudioVolume;    
+    [CanBeNull, Tooltip("AudioVolume skilla -> Target"), SerializeField] public float skill_OnTargetHitAudioVolume;
+    [Space]
+    [CanBeNull, Tooltip("Prefab z VisualEffectem pojawiający się na przeciwniku -> Target "), SerializeField] public GameObject skill_OnTargetHitVisualEffectPrefab;   
+    //Można tak zrobić że jak trafi przeciwnika to pojawia na nim (Instantiate) empty object(Prefab z Bazy) który ma na sobie VisualEffect ale może pojawiać się tylko raz
+    //Można mu dać że ma zniknąć jeśli nie jest na liście targetów castującego
+      
 
     [Header("Animator Values")]
     [CanBeNull, Tooltip("Jaki Float Animatora odpowiada za animację? - Nazwa"), SerializeField] public string skill_AnimatorFloatName;
     [CanBeNull, Tooltip("Jaki Trigger Animatora odpowiada za animację?  - Nazwa"), SerializeField] public string skill_AnimatorTriggerName;
     [CanBeNull, Tooltip("Jaki Bool Animatora odpowiada za animację? - Nazwa "), SerializeField] public string skill_AnimatorBoolName;
+    [CanBeNull, Tooltip("Jaki Trigger Animatora przy finish casta?  - Nazwa"), SerializeField] public string skill_AnimatorTriggerOnFinishedCastingName;
+    [Space]
+    [CanBeNull, Tooltip("Jaki Float Animatora zastopować przy isCasting? - Nazwa"), SerializeField] public string skill_StopOnCastFloatNameAnimator;
+    [CanBeNull, Tooltip("Jaki Trigger Animatora zastopować przy isCasting?  - Nazwa"), SerializeField] public string skill_StopOnCastTriggerNameAnimator;
+    [CanBeNull, Tooltip("Jaki Bool Animatora zastopować przy isCasting? - Nazwa "), SerializeField] public string skill_StopOnCastBoolNameAnimator;
+    
+    
 
-   /* [CanBeNull, Tooltip("Na którym combo colider działa?"), SerializeField] public float skill_onCombo;
+    /* [CanBeNull, Tooltip("Na którym combo colider działa?"), SerializeField] public float skill_onCombo;
 
-    [Header("SkillVariable VFX")]
-    [Tooltip("VFX - Ilość particali"), SerializeField] public float skill_ParticlesPerSec;
-    [Tooltip("VFX - Minimalny Lifetime particali"), SerializeField] public float skill_LifetimeMin;
-    [Tooltip("VFX - Maxymalny Lifetime particali"), SerializeField] public float skill_LifetimeMax;
-    [Tooltip("VFX - Kierunek i prędkość (wektor) rozchodzenia się particali"), SerializeField] public float skill_MovingTowardsFactor;*/
+     [Header("SkillVariable VFX")]
+     [Tooltip("VFX - Ilość particali"), SerializeField] public float skill_ParticlesPerSec;
+     [Tooltip("VFX - Minimalny Lifetime particali"), SerializeField] public float skill_LifetimeMin;
+     [Tooltip("VFX - Maxymalny Lifetime particali"), SerializeField] public float skill_LifetimeMax;
+     [Tooltip("VFX - Kierunek i prędkość (wektor) rozchodzenia się particali"), SerializeField] public float skill_MovingTowardsFactor;*/
 
 }
