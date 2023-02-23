db6c1dd1267d1b08b2c39551266efdb3007ca1fc
Remo <Remczas@interia.pl>
2023-02-23 22:32:44 +0100

    Update ScrObj_skill.cs
    Usuwanie zbędnych zmiennych, zmiana nazw zmiennych i formatowanie tekstu

--- 3D Game My project/Assets/Script/ScriptableAssetsScripts/ScrObj_skill.cs
+++ 3D Game My project/Assets/Script/ScriptableAssetsScripts/ScrObj_skill.cs
@@ -9,80 +9,32 @@ using UnityEngine.VFX;
 [CreateAssetMenu(fileName = "SpellInfo", menuName = "Chars/SpellInfo")]
 public class ScrObj_skill : ScriptableObject
 {
-    public enum Skill_InputType { primary, secondary }
-    [Tooltip("Jaki przycisk aktywuje skilla?")] public Skill_InputType skill_InputType;
+    public enum InputType { primary, secondary }
+    [Tooltip("Jaki przycisk aktywuje skilla?")] public InputType _inputType;
 
-    public enum Skill_CastingType { Castable, Instant, Hold }
-    [Tooltip("Który rodzaj castowania aktywuje SkillEfect?"), HideInInspector] public Skill_CastingType skill_CastingType;
+    public enum CastingType { Instant, Hold, Castable }
+    [Tooltip("Który rodzaj castowania aktywuje SkillTarget ?")] public CastingType _castingType;    
 
-    public enum Skill_TargetType { melee, cone, projectile, aoeMouse, self, summon }
-    [Tooltip("Na jakie targety działa SkillEffect?")] public Skill_TargetType skill_TargetType;
+    public enum ResourceType { health, mana, stamina }
+    [Tooltip("Jaki Resource zużywa do Skilla?")] public ResourceType _resourceType;
 
-    public enum Skill_ResourceType { health, mana, stamina }
-    [Tooltip("Jaki Resource zużywa do Skilla?")] public Skill_ResourceType _resourceType;
+    public enum EffectType { None, Hit, DamageOverTime, Heal, HealOverTime, Summon }
 
-    public enum Skill_EffectTypeArray { none, hit, boom, pierce, chain, damageOverTime, healOverTime, heal, summon }
-    [Tooltip("Jaki SkillEffect jeset użyty na Targetach?") ,PropertyAttribute_EnumNamedArray(typeof(Skill_CastingType))] public Skill_EffectTypeArray[] skill_EffectTypeArray = new Skill_EffectTypeArray[3]; //Max 3 elementy
+    public enum TargetType { None, ConeOnCombo, ConeOnTime, Projectile, AreaOfEffectMouse, Self, Chain, Pierce, Boom }
 
     [Space]
-    [Header("SkillRange")]
-    [Tooltip("Bazowy Minimalny Radius skilla")] public float skill_MinRadius;
-    [Tooltip("Bazowy Minimalny Kąt skilla")] public float skill_MinAngle;
+    [Header("SkillRange - wymagany")]
     [Tooltip("Bazowy Maxymalny Radius skilla. \n Wykorzystany w SkillForge jako OverlapSphereNonAlloc.Range dla wszystkich targetTypów skilla \n oraz do FielOfView SpellRangeChecha")] public float _skillMaxRadius;
-    [Tooltip("Bazowy Maxymalny Kąt skilla")] public float skill_MaxAngle;
 
     [Header("SkillTime")]
-    [Tooltip("Ile czasu potrzeba do Max/Min Radius")] public float skill_TimeMaxRadius;
-    [Tooltip("Ile czasu potrzeba do Max/Min Angle")] public float skill_TimeMaxAngle;
-    [Tooltip("Ile czasu potrzeba do Deploy Cast")] public float skill_TimeCast;
-    [Tooltip("Bazowy Cooldown skilla")] public float skill_BaseCooldown;
+    [Tooltip("Ile czasu potrzeba do Deploy Cast")] public float _timeCast;
+    [Tooltip("Bazowy Cooldown skilla")] public float _baseCooldown;
 
     [Header("SkillDamage/Cost")]
     [Tooltip("Bazowy Resource cost skilla")] public float _baseResourceCost;
-    [Tooltip("Bazowy Damage skilla")] public float skill_BaseDamage;    
     [Tooltip("Multiplier do Levela i BonusCharStats do obliczeń [0.1f] -> na każdy lev i na każdy BonusDMG dodaje 0.1f BAZOWYCH DMG")] public float _multiplier;
 
-    [Header("SkillObstacles")]
-    [Tooltip("Layer Mask z przeszkodami przez które nie da się atakować(z klasy skill)")] public LayerMask skill_ObstaclesMask; //Obstacles dla skilla
-
-    [Header("SkillAudio")]
-    [CanBeNull, Tooltip("AudioClip skilla OneShotOverlap -> Caster")] public AudioClip skill_OneShotOverlapAudioClip;
-    [CanBeNull, Tooltip("AudioClip skilla OneShot non-Overlap -> Caster")] public AudioClip skill_OneShotNonOverlapAudioClip;   
-    [CanBeNull, Tooltip("AudioClip skilla TimeCastOverlap -> Caster")] public AudioClip skill_TimeCastOverlapAudioClip;
-    [CanBeNull, Tooltip("AudioClip skilla TimeCast Non-Overlap -> Caster")] public AudioClip skill_TimeCastNonOverlapAudioClip;
-    [CanBeNull, Tooltip("AudioClip skilla OnFinished -> Caster")] public AudioClip skill_OnFinishCastingAudioClip;
-    [CanBeNull, Tooltip("Czas Loopa skilla Hold -> Caster")] public float skill_HoldAudioLoopTime;
-    [Space]
-    [CanBeNull, Tooltip("AudioClip skilla -> Target")] public AudioClip skill_OnTargetHitAudioClip;
-    [Space]
-    [CanBeNull, Tooltip("AudioVolume skilla -> Caster")] public float skill_CasterAudioVolume;
-    [CanBeNull, Tooltip("AudioVolume skilla -> Target")] public float skill_OnTargetHitAudioVolume;
-    [Space]
-    [CanBeNull, Tooltip("Prefab z VisualEffectem pojawiający się na przeciwniku -> Target ")] public GameObject skill_OnTargetHitVisualEffectPrefab;
-    //Można tak zrobić że jak trafi przeciwnika to pojawia na nim (Instantiate) empty object(Prefab z Bazy) który ma na sobie VisualEffect ale może pojawiać się tylko raz
-    //Można mu dać że ma zniknąć jeśli nie jest na liście targetów castującego    
-
-    
-    
-    [Header("Animator Values")]
-    [CanBeNull, Tooltip("Jaki Float Animatora odpowiada za animację? - Nazwa")] public string skill_AnimatorFloatName;
-    [CanBeNull, Tooltip("Jaki Trigger Animatora odpowiada za animację?  - Nazwa")] public string skill_AnimatorTriggerName;
-    [CanBeNull, Tooltip("Jaki Bool Animatora odpowiada za animację? - Nazwa ")] public string skill_AnimatorBoolName;
-    [CanBeNull, Tooltip("Jaki Trigger Animatora przy finish casta?  - Nazwa")] public string skill_AnimatorTriggerOnFinishedCastingName;
-    [Space]
-    [CanBeNull, Tooltip("Jaki Float Animatora zastopować przy isCasting? - Nazwa")] public string skill_StopOnCastFloatNameAnimator;
-    [CanBeNull, Tooltip("Jaki Trigger Animatora zastopować przy isCasting?  - Nazwa")] public string skill_StopOnCastTriggerNameAnimator;
-    [CanBeNull, Tooltip("Jaki Bool Animatora zastopować przy isCasting? - Nazwa ")] public string skill_StopOnCastBoolNameAnimator;
-
-    /* [CanBeNull, Tooltip("Na którym combo colider działa?")] public float skill_onCombo;
-
-     [Header("SkillVariable VFX")]
-     [Tooltip("VFX - Ilość particali")] public float skill_ParticlesPerSec;
-     [Tooltip("VFX - Minimalny Lifetime particali")] public float skill_LifetimeMin;
-     [Tooltip("VFX - Maxymalny Lifetime particali")] public float skill_LifetimeMax;
-     [Tooltip("VFX - Kierunek i prędkość (wektor) rozchodzenia się particali")] public float skill_MovingTowardsFactor;*/
-
-   /* [Flags] public enum New_EnumEffectType
+    /* [Flags] public enum New_EnumEffectType
     { 
         none = 0,
         hit = 2,
@@ -91,51 +43,21 @@ public class ScrObj_skill : ScriptableObject
         healOverTime = 16,
         summon = 32
     }*/
-    
-    public enum New_EnumEffectType
-    { 
-        None,
-        Hit,
-        DamageOverTime,
-        Heal,
-        HealOverTime,
-        Summon
-    }
-    public enum New_EnumCastingType 
-    {
-        Instant,
-        Hold,
-        Castable
-    }
-
-    public enum New_EnumTargetType 
-    { 
-        None,
-        Melee,
-        Cone,
-        Projectile,
-        AreaOfEffectMouse,
-        Self,
-        Chain,
-        Pierce,
-        Boom
-    }
-    
 
     [Serializable]
-    public class New_EffectType
-    {    
-        [Tooltip("Jaki SkillEffect jeset użyty na Targetach?")] public New_EnumEffectType new_EnumEffectType;        
+    public class EffectTypes
+    {
+        [Tooltip("Jaki SkillEffect jeset użyty na Targetach?")] public EffectType _effectType;
 
-        [Header("SkillDamage")]        
-        [Tooltip("Bazowy Damage skilla")] public float _baseDamage;        
+        [Header("SkillDamage")]
+        [Tooltip("Bazowy Damage skilla")] public float _baseDamage;
     }
 
     [Serializable]
-    public class New_TargetType
+    public class TargetTypes
     {
-        [Tooltip("Na jakie targety działa SkillEffect?")] public New_EnumTargetType new_EnumTargetType;
-        [Tooltip("Jaki SkillEffect jeset użyty na Targetach?")] public New_EffectType[] new_EffectType;
+        [Tooltip("Na jakie targety działa SkillEffect?")] public TargetType _targetType;
+        [Tooltip("Jaki SkillEffect jeset użyty na Targetach?")] public EffectTypes[] _effectTypes;
 
         [Header("SkillRange")]
         [Tooltip("Bazowy Minimalny Radius skilla")] public float _minRadius;
@@ -146,27 +68,45 @@ public class ScrObj_skill : ScriptableObject
         [Header("SkillTime")]
         [Tooltip("Ile czasu potrzeba do Max/Min Radius")] public float _timeMaxRadius;
         [Tooltip("Ile czasu potrzeba do Max/Min Angle")] public float _timeMaxAngle;
-        
+
         [Header("SkillObstacles")]
         [Tooltip("Layer Mask z przeszkodami przez które nie da się atakować(z klasy skill)")] public LayerMask _obstaclesMask; //Obstacles dla skilla           
     }
-    [Tooltip("Który rodzaj castowania aktywuje SkillEfect ?")] public New_EnumCastingType new_EnumCastingType;
-    [Tooltip("Na jakie targety działa SkillEffect?")] public New_TargetType[] new_TargetType;
+
+    [Tooltip("Na jakie targety działa SkillEffect?")] public TargetTypes[] _targetTypes;
     [Space]
     [Tooltip("Maxymalna ilość TargetTypów dla skilla")] public int maxTargetTypes;
     [Tooltip("Maxymalna ilość EffectTypów dla pojedynczego TargetType")] public int maxEffectTypes;
 
-    #region TestingCustomPropertyArray
-    [Serializable]
-    public class CosixClass
-    {
-        [Header("SkillRange")]
-        [Tooltip("Bazowy Minimalny Radius skilla")] public float flaot_1;
-        [Tooltip("Bazowy Minimalny Kąt skilla")] public float flaot_2;
-        [Tooltip("Bazowy Maxymalny Radius skilla")] public float flaot_3;
-        [Tooltip("Bazowy Maxymalny Kąt skilla")] public float flaot_4;
-    }
-    [PropertyAttribute_EnumNamedNestedArray(typeof(Skill_CastingType)), HideInInspector] public CosixClass[] cosixClasses = new CosixClass[3];
-    //public CosixClass cosixClasses;
-    #endregion
+    [Space]
+    [Header("SkillAudio")]
+    [CanBeNull, Tooltip("AudioClip skilla -> Caster")] public AudioClip _casterAudioClip;
+    [CanBeNull, Tooltip("AudioClip skilla OnFinished -> Caster")] public AudioClip _onFinishCastingAudioClip;
+    [Space]
+    [CanBeNull, Tooltip("AudioClip skilla -> Target")] public AudioClip _onTargetHitAudioClip;
+    [Space]
+    [CanBeNull, Tooltip("AudioVolume skilla -> Caster")] public float _casterAudioVolume;
+    [CanBeNull, Tooltip("AudioVolume skilla -> Target")] public float _onTargetHitAudioVolume;
+    [Space]
+    [CanBeNull, Tooltip("Prefab z VisualEffectem pojawiający się na przeciwniku -> Target ")] public GameObject _onTargetHitVisualEffectPrefab;
+    //Można tak zrobić że jak trafi przeciwnika to pojawia na nim (Instantiate) empty object(Prefab z Bazy) który ma na sobie VisualEffect ale może pojawiać się tylko raz
+    //Można mu dać że ma zniknąć jeśli nie jest na liście targetów castującego 
+
+    [Header("Animator Values")]
+    [CanBeNull, Tooltip("Jaki Float Animatora odpowiada za animację? - Nazwa")] public string _animatorFloatName;
+    [CanBeNull, Tooltip("Jaki Trigger Animatora odpowiada za animację?  - Nazwa")] public string _animatorTriggerName;
+    [CanBeNull, Tooltip("Jaki Bool Animatora odpowiada za animację? - Nazwa ")] public string _animatorBoolName;
+    [CanBeNull, Tooltip("Jaki Trigger Animatora przy finish casta?  - Nazwa")] public string _animatorTriggerOnFinishedCastingName;
+    [Space]
+    [CanBeNull, Tooltip("Jaki Float Animatora zastopować przy isCasting? - Nazwa")] public string _stopOnCastFloatNameAnimator;
+    [CanBeNull, Tooltip("Jaki Trigger Animatora zastopować przy isCasting?  - Nazwa")] public string _stopOnCastTriggerNameAnimator;
+    [CanBeNull, Tooltip("Jaki Bool Animatora zastopować przy isCasting? - Nazwa ")] public string _stopOnCastBoolNameAnimator;
+
+    /* [CanBeNull, Tooltip("Na którym combo colider działa?")] public float skill_onCombo;
+
+     [Header("SkillVariable VFX")]
+     [Tooltip("VFX - Ilość particali")] public float skill_ParticlesPerSec;
+     [Tooltip("VFX - Minimalny Lifetime particali")] public float skill_LifetimeMin;
+     [Tooltip("VFX - Maxymalny Lifetime particali")] public float skill_LifetimeMax;
+     [Tooltip("VFX - Kierunek i prędkość (wektor) rozchodzenia się particali")] public float skill_MovingTowardsFactor;*/      
 }
