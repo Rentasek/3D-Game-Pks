f6f1fa0a270ee2949cfce9d2e0b7c6c8ce456a89
Remo <Remczas@interia.pl>
2023-02-24 11:36:27 +0100

    Update CharacterBonusStats.cs
    1. Dopisanie Tooltipów
    2. Dołożenie Regena

--- 3D Game My project/Assets/Script/Characters/CharacterBonusStats.cs
+++ 3D Game My project/Assets/Script/Characters/CharacterBonusStats.cs
@@ -4,33 +4,32 @@ public class CharacterBonusStats : MonoBehaviour
 {    
     [Space]
     [Header("Character Speed")]
-    public float bonus_currentWalkSpeed;
-    public float bonus_currentRunSpeed;
-    //public float bonus_currentJumpPower;
-    //public float bonus_currentJumpStamCost; //wpisywaæ wartoœci ujemne
+    [Tooltip("Bonus do WalkSpeed")] public float bonus_currentWalkSpeed;
+    [Tooltip("Bonus do RunSpeed - wylicznay z walkSpeed")] public float bonus_currentRunSpeed;
 
     [Space]
     [Header("Character Melee Combat")]
-    //public float bonus_currentAttackCooldown;
-    public float bonus_currentDamageCombo;
-    //public float bonus_currentAttackStamCost;   //wpisywaæ wartoœci ujemne
+    [Tooltip("Bonus Damage CloseRangeSkilla")] public float bonus_CloseRangeDamage;
     
     [Space]
     [Header("Character Magic Combat")]
-    public float bonus_Skill_Damage; //zwiêkszaæ o 0.1f
-    //public float bonus_breath_MPCost;  //wpisywaæ wartoœci ujemne
+    [Tooltip("Bonus Damage SpellRangeSkilla")] public float bonus_SpellRangeDamage; //zwiêkszaæ o 0.1f
     [Space, Header("Regen From Orbs")]
-    public float percentRegen;
+    [Tooltip("Ile MaxResource zostanie zwrócone po podniesieniu ResourceOrba"), Range(0f, 1f)] public float percentRegen;
 
     [Space]
     [Header("Character Stats")]
-    public float bonus_currentMaxHP;
-    public float bonus_currentMaxMP;
-    public float bonus_currentMaxStam;
-    
-    
+    [Tooltip("Bonus do Max HP")] public float bonus_currentMaxHP;
+    [Tooltip("Bonus do Regenu HP")] public float bonus_currentRegenHP;
+
+    [Tooltip("Bonus do Max MP")] public float bonus_currentMaxMP;
+    [Tooltip("Bonus do Regenu MP")] public float bonus_currentRegenMP;
+
+    [Tooltip("Bonus do Max Staminy")] public float bonus_currentMaxStam;
+    [Tooltip("Bonus do Regenu Staminy")] public float bonus_currentRegenStam;
+
     [Space]
     [Header("Skill Points")]
-    public float bonus_SkillPoints;    
+    [Tooltip("Ile jest dostêpnych SkillPoints?")] public float bonus_SkillPoints;    
 
 }
