30fe87c67dfcd4592d153e2a3a2119cd4e42d1c1
Remo <Remczas@interia.pl>
2023-03-01 00:52:11 +0100

    Poprawa targetType Cone i Chain (wywalenie remove target)
    Wyłączyłem część kodu odpowiadającą za Remove  z targetList.
    Przez to przy 2 uderzeniu melee (Cone) usuwało target z 1-wszego uderzenia i dodawało tylko nowy jeśli znalazł się w zasięgu.

--- 3D Game My project/Assets/Script/Spells/SkillForge.cs
+++ 3D Game My project/Assets/Script/Spells/SkillForge.cs
@@ -328,11 +328,11 @@ public static class SkillForge
                                     {
                                         skill.targetDynamicValues[targetTypeIndex]._targetColliders.Add(skill._allLocalColliders[i]); //przypisuje do listy colliders jeśli ma taga z listy enemies                                    
                                     }
-                                    else
+                                    /*else
                                     {
                                         skill.targetDynamicValues[targetTypeIndex]._targetColliders.Remove(skill._allLocalColliders[i]);
                                         if (skill.targetDynamicValues[targetTypeIndex]._targetColliders.Count <= 0) skill.targetDynamicValues[targetTypeIndex]._targetInAngle = false;  //jeśli nie ma żadnych targetów w Cone Angle
-                                    }
+                                    }*/
                                 }
                                 else
                                 {
@@ -514,11 +514,11 @@ public static class SkillForge
                                 {
                                     skill.targetDynamicValues[targetTypeIndex]._targetColliders.Add(skill._allLocalColliders[i]); //przypisuje do listy colliders jeśli ma taga z listy enemies  
                                 }
-                                else
+                                /*else
                                 {
                                     skill.targetDynamicValues[targetTypeIndex]._targetColliders.Remove(skill._allLocalColliders[i]);
                                     if (skill.targetDynamicValues[targetTypeIndex]._targetColliders.Count <= 0) skill.targetDynamicValues[targetTypeIndex]._targetInAngle = false;  //jeśli nie ma żadnych targetów w Cone Angle
-                                }
+                                }*/
                             }
                             else { skill.targetDynamicValues[targetTypeIndex]._targetColliders.Remove(skill._allLocalColliders[i]); }
                         }
@@ -540,11 +540,11 @@ public static class SkillForge
                                         {
                                             skill.targetDynamicValues[targetTypeIndex]._targetColliders.Add(skill._allLocalColliders[i]); //przypisuje do listy colliders jeśli ma taga z listy enemies        
                                         }
-                                        else
+                                        /*else
                                         {
                                             skill.targetDynamicValues[targetTypeIndex]._targetColliders.Remove(skill._allLocalColliders[i]);
                                             //if (skill.targetDynamicValues[targetTypeIndex]._targetColliders.Count <= 0) skill.targetDynamicValues[targetTypeIndex]._targetInAngle = false;  //jeśli nie ma żadnych targetów w Cone Angle
-                                        }
+                                        }*/
                                     }
                                     else
                                     {
